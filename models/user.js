36579f9611c0686b0e1e460336a383379fee6f79
wahibhanii <wahib.hanii@gmail.com>
2017-12-08 01:41:57 +0700

    add cancel

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: true 
+      primaryKey: true,
+      autoIncrement: true
     },
     email   : {
       type     : DataTypes.STRING,
